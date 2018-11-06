1d2151078f80c5e7d78673ccdb89885ba945df32
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2018-11-06 11:52:07 +0100

    Improve e01

--- codeflix/onecode/season.04/e01.js
+++ codeflix/onecode/season.04/e01.js
@@ -9,46 +9,42 @@ const PORT = process.argv[2]
 let usersData = {}
 
 http.createServer(function (req, res) {
-    if (req.url.startsWith('/?n=')) {
-        // Get user ip to store number and trys individually
-        const userIp = (req.headers['x-forwarded-for'] || '').split(',').pop() ||
-            req.connection.remoteAddress ||
-            req.socket.remoteAddress ||
-            req.connection.socket.remoteAddress
+    // Get user ip to store number and trys individually
+    const userIp = (req.headers['x-forwarded-for'] || '').split(',').pop() ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress
 
-        // Init number and user try if necessary
-        if (!usersData[userIp])
-            usersData[userIp] = { number: -1, try: 0 }
+    // Init number and user try if necessary
+    if (!usersData[userIp])
+        usersData[userIp] = { number: -1, try: 0 }
 
-        const userData = usersData[userIp]
+    const userData = usersData[userIp]
 
-        if (userData.number == -1)
+    if (userData.number == -1)
         userData.number = Math.floor(Math.random() * Math.floor(1000));
 
-        // Number that user is trying
-        const number = parseInt(url.parse(req.url, true).query.n)
+    // Number that user is trying
+    const queryN = url.parse(req.url, true).query.n
+    const number = parseInt(queryN)
 
-        res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
 
-        // Check if user has correctly provide his number
-        if (!number) {
-            res.write("You must provide an integer in 'n' parameter of the url, example: /?n=500")
-        } else {
-            // If the user found the number
-            userData.try++
-            if (userData.number == number) {
-                res.write(`Well done! You have found in ${userData.try} shots`)
-                userData.try = 0
-                userData.number = -1
-            } else if (userData.number > number) {
-                res.write(`Bigger`)
-            } else {
-                res.write(`Smaller`)
-            }
-        }
+    // Check if user has correctly provide his number
+    if (!queryN || !number) {
+        res.write("You must provide an integer in 'n' parameter of the url, example: /?n=500")
     } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' })
-        res.write('404 Not Found')
+        // If the user found the number
+        userData.try++
+        if (userData.number == number) {
+            res.write(`Well done! You have found in ${userData.try} shots`)
+            userData.try = 0
+            userData.number = -1
+        } else if (userData.number > number) {
+            res.write(`Bigger`)
+        } else {
+            res.write(`Smaller`)
+        }
     }
     res.end()
 }).listen(PORT)
