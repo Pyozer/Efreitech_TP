0299e907b4da6fae86b20909c91c2a85abdcc181
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2018-12-04 15:19:16 +0100

    Improve app and pokemoninfocard component

--- react/pokereact/pokedex-client/src/App.js
+++ react/pokereact/pokedex-client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import logoPokedex from './assets/Pokedex_logo.png';
 import Home from './pages/Home'
@@ -6,23 +6,19 @@ import Pokemon from './pages/Pokemon'
 import PageNotFound from './pages/PageNotFound'
 import './css/App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="container">
-          <header className="App-header">
-            <img src={logoPokedex} className="App-logo" alt="logo" />
-          </header>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/:pokemonId" exact component={Pokemon} />
-            <Route component={PageNotFound} />
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div className="container">
+      <header className="App-header">
+        <img src={logoPokedex} className="App-logo" alt="logo" />
+      </header>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/:pokemonId" exact component={Pokemon} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </div>
+  </Router>
+)
 
 export default App;
--- react/pokereact/pokedex-client/src/components/PokemonInfosCard.js
+++ react/pokereact/pokedex-client/src/components/PokemonInfosCard.js
@@ -1,23 +1,30 @@
 import React from 'react'
 
-const PokemonInfosCard = ({ data }) => (
-    <div className="card">
-        <div className="card-content">
-            <table>
-                <tbody>
-                    {Object.entries(data).map(([key, value]) => {
-                        if (!value) return <></>
-                        return (
-                            <tr key={key}>
-                                <td className="capitalize">{key}</td>
-                                <td className="capitalize">{value}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
+const PokemonInfosCard = ({ data }) => {
+    let tableLines = []
+    Object.entries(data).forEach(([key, value]) => {
+        if (value)
+            tableLines.push(
+                <tr key={key}>
+                    <td className="capitalize">{key}</td>
+                    <td className="capitalize">{value}</td>
+                </tr>
+            )
+    })
+
+    if (tableLines.length === 0) return <></>
+
+    return (
+        <div className="card">
+            <div className="card-content">
+                <table>
+                    <tbody>
+                        {tableLines}
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
 export default PokemonInfosCard
