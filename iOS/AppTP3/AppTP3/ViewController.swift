560925647a2e51cdbc7371ad0de930d8f74ce0e6
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2019-03-04 10:47:50 +0100

    Add iOS TP3

---
+++ iOS/AppTP3/AppTP3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppTP3
+//
+//  Created by Jean-Charles Moussé on 13/02/2019.
+//  Copyright © 2019 Jean-Charles Moussé. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ iOS/AppTP3/AppTP3Tests/AppTP3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  AppTP3Tests.swift
+//  AppTP3Tests
+//
+//  Created by Jean-Charles Moussé on 13/02/2019.
+//  Copyright © 2019 Jean-Charles Moussé. All rights reserved.
+//
+
+import XCTest
+@testable import AppTP3
+
+class AppTP3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
